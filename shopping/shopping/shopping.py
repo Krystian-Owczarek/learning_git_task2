77b570d15c278e835c257ce61b03ca57079e5c45
Krystian Owczarek <k.owczarek4848@gmail.com>
2022-05-21 17:08:38 +0200

    Shopping

---
+++ shopping/shopping/shopping.py
@@ -0,0 +1,14 @@
+shopping_dict = {}
+shopping_dict['piekarnia'] = ["chleb", "bułki", "pączek"]
+shopping_dict['warzywniak'] = ["marchew", "seler", "rukola"]
+number_of_values = 0
+
+for elem in shopping_dict:
+  products = shopping_dict.get(elem)
+  products_capitalized = []
+  for product in products:
+    products_capitalized.append(product.capitalize())
+    number_of_values += 1
+  print(f'Idę do {elem.title()} i kupuję tam {products_capitalized}')
+
+print(f'W sumie kupuję {number_of_values} produktów')
